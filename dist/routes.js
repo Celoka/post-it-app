e6efa1ced703a80cd641f2683ac8e1f668b6f85f
Celoka <eloka.chima@gmail.com>
2017-05-26 09:33:11 +0100

    Edited package.json file to enable it run on heroku

--- dist/routes.js
+++ dist/routes.js
@@ -1,6 +1,6 @@
 const express = require('express'),
-  firebase = require('firebase'),
-  router = express.Router();
+      firebase = require('firebase'),
+      router = express.Router();
 
 const config = {
   apiKey: 'AIzaSyAejYE9GO5kC-DIwE1sJBbFfE1mT5kR_-M',
@@ -18,60 +18,55 @@ router.post('/user/signup', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const username = req.body.username;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(() => {
-      userRef.push({
-        userEmail: email,
-        userPassword: password,
-        userName: username
-      });
-      res.send({
-        message: 'Successfully created!'
-      });
-    })
-    .catch((error) => {
-      res.send({
-        message: error.message
-      });
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+    userRef.push({
+      userEmail: email,
+      userPassword: password,
+      userName: username
     });
+    res.send({
+      message: 'Successfully created!'
+    });
+  }).catch(error => {
+    res.send({
+      message: error.message
+    });
+  });
 });
 // -----------------------------------------Signin Route---------
 router.post('/user/signin', (req, res) => {
   const password = req.body.password;
   const email = req.body.email;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-
-    .then(() => {
-      userRef.push({
-        userPassword: password,
-        userEmail: email
-      });
-      res.send({
-        message: 'User Signed in!'
-      });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      if (errorCode === 'auth/wrong password') {
-        res.send('Wrong password or email');
-      } else {
-        res.send({
-          message: `Enter valid email and password${errorMessage}`
-        });
-      }
+  firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+    userRef.push({
+      userPassword: password,
+      userEmail: email
     });
+    res.send({
+      message: 'User Signed in!'
+    });
+  }).catch(error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    if (errorCode === 'auth/wrong password') {
+      res.send('Wrong password or email');
+    } else {
+      res.send({
+        message: `Enter valid email and password${errorMessage}`
+      });
+    }
+  });
 });
 // ------------------------------------------------Group Route---------
 router.post('/group', (req, res) => {
   const groupName = req.body.groupname;
-// checking if user is signed in
-  firebase.auth().onAuthStateChanged((user) => {
+  // checking if user is signed in
+  firebase.auth().onAuthStateChanged(user => {
     if (user) {
       const userId = user.uid;
       db.ref().child('group').push({
         groupname: groupName,
-        groupOwner: userId,
+        groupOwner: userId
       });
       res.send({
         message: 'Group created successfully'
@@ -86,8 +81,7 @@ router.post('/group', (req, res) => {
 // ----------addUserToGroup-----------
 router.post('/group/:groupId/user', (req, res) => {
   const groupKey = req.params.groupId;
-  firebase.database().ref(`Groups/${groupKey}/groupMembers/`)
-  .push({ user: req.body.mail });
+  firebase.database().ref(`Groups/${groupKey}/groupMembers/`).push({ user: req.body.mail });
   res.send('user added');
 });
 module.exports = router;
---
+++ dist/server.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 6969,
+      bodyParser = require('body-parser'),
+      routes = require('./routes');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to Post IT!');
+});
+
+app.use(routes);
+app.listen(port);
+console.log(`listening on ${port}`);
