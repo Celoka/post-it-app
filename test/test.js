95c77874f6d68f5275845177a2ce375ebaf7ced3
Celoka <eloka.chima@gmail.com>
2017-06-02 10:07:27 +0100

    Wrote more test for routes

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
-const server = require('../server/server');
+const server = require('../dist/server');
+
 // UNIT test begin
 describe('API route unit test', () => {
   it('should create a new user', (done) => {
@@ -13,29 +14,29 @@ describe('API route unit test', () => {
        done();
      });
   });
-});
 
-// it('should sign in a user', (done) => {
-//     server
-//       .post('user/signin')
-//       .send({ email: 'andela@yahoo.com', password: 'Luska1' })
-//       .expect('Content-type', /firebase/)
-//       .expect(200)
-//       .end((err, res) => {
-//         res.status.should.equal(200);
-//         res.status.error.should.equal(false);
-//         res.body.data.should.equal(email, password);
-//         done();
-//       });
-//   });
-//    it('should sign out a user from account', (done) => {
-//      server
-//      .post('user/signout')
-//       .expect('Signed out!')
-//       .expect(200)
-//       .end((err, res) => {
-//         res.status.should.equal(200);
-//         res.body.error.should.equal(false);
-//       });
-//    });
-//  });
+  it('should sign in a user', (done) => {
+    supertest(server)
+      .post('user/signin')
+      .send({ email: 'andela@yahoo.com', password: 'Luska1' })
+      .expect(200)
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.status.error.should.equal(false);
+        res.body.data.should.equal(email, password);
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should sign out a user from account', (done) => {
+    supertest(server)
+     .post('user/signout')
+      .expect('Signed out!')
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
